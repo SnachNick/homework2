f913229c57d86943d408ba709bf7d717d96124e8
sasha_pereverzev <pereverzev.sasha@gmail.com>
2023-04-17 18:58:07 +0300

    the lst one

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.AspNetCore.Builder;
 using NCars.Classes;
 using System;
 using System.Runtime.CompilerServices;
@@ -14,27 +15,31 @@ builder.Services.AddControllers();
 builder.Services.AddTransient<ICar, CarControl>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//--builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
 var car = app.Services.GetService<ICar>();
 
+
+
+var ta = car.GetCarAgeL();
+var tc = car.GetCarNameL();
+var te = car.GetCarEngineL();
+
 app.Map("/Cars", appM =>
     {
         appM.Run(async context =>
         {
-            var tc = car.GetCarNameL();
             foreach (var c in tc)
-                await context.Response.WriteAsync($"{c} \n");
+            await context.Response.WriteAsync($"{c} \n");
         });
     });
 app.Map("/Engine", appM =>
 {
     appM.Run(async context =>
     {
-        var tc = car.GetCarEngineL();
-        foreach (var c in tc)
+        foreach (var c in te)
             await context.Response.WriteAsync($"{c} \n");
     });
 });
@@ -42,24 +47,37 @@ app.Map("/CarsAges", appM =>
 {
     appM.Run(async context =>
     {
-        var tc = car.GetCarAgeL();
-        foreach (var c in tc)
+        
+        foreach (var c in ta)
             await context.Response.WriteAsync($"{c} \n");
     });
 });
 
-
+/*
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+*/
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+//app.MapControllers();
+
+
+app.Run(async (context) =>
+{
+
+    foreach (var c in ta)
+        await context.Response.WriteAsync($"{c} \n");
+    foreach (var c in tc)
+        await context.Response.WriteAsync($"{c} \n");
+    foreach (var c in te)
+        await context.Response.WriteAsync($"{c} \n");
+}
+);
 
 app.Run();
