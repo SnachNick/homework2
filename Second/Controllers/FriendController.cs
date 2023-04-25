5a135f665927b6e2d21ca4f091cfdf89c344830a
sasha_pereverzev <pereverzev.sasha@gmail.com>
2023-04-25 22:42:50 +0300

    not done

---
+++ Second/Controllers/FriendController.cs
@@ -0,0 +1,23 @@
+﻿using Second.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Second.Controllers
+{
+    public class FriendController : Controller
+    {
+        // GET: Friend
+        public ActionResult Index()
+        {
+            return View();
+        }
+      //  [HttpPost]
+        public ActionResult View()
+        {
+            return View("View");
+        }
+    }
+}
---
+++ Second/Models/Friend.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Second.Models
+{
+    public class Friend
+    {
+        [Required]
+        public int FriendID {get; set;}
+        [MinLength(1, ErrorMessage = "Friend Name Empty Not Allowed")]
+        public string FriendName { get; set;}
+        [StringLength(50, ErrorMessage = "The leng of the Place shouldn't be more than 25 characters")]
+        public string Place { get; set;}
+    }
+}
